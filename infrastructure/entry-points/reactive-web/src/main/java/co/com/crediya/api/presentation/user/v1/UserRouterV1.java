8255b0453b622a75f8e3d17879267ea2fe464eb0
Jenner369 <64997441+Jenner369@users.noreply.github.com>
2025-09-03 17:28:28 -0500

    feat: Add endpoint to find all by identity documents

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/presentation/user/v1/UserRouterV1.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/presentation/user/v1/UserRouterV1.java
@@ -2,6 +2,7 @@ package co.com.crediya.api.presentation.user.v1;
 
 import co.com.crediya.api.presentation.user.v1.handler.GetUserByIdHandlerV1;
 import co.com.crediya.api.presentation.user.v1.handler.GetUserByIdentityDocumentHandlerV1;
+import co.com.crediya.api.presentation.user.v1.handler.ListUsersByIdentityDocumentsHandlerV1;
 import co.com.crediya.api.presentation.user.v1.handler.RegisterUserHandlerV1;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
@@ -34,18 +35,26 @@ public class UserRouterV1 {
                     beanClass = GetUserByIdentityDocumentHandlerV1.class,
                     beanMethod = "handle",
                     method = RequestMethod.GET
+            ),
+            @RouterOperation(
+                    path = "/api/v1/usuarios/identity-documents",
+                    beanClass = ListUsersByIdentityDocumentsHandlerV1.class,
+                    beanMethod = "handle",
+                    method = RequestMethod.POST
             )
     })
     public RouterFunction<ServerResponse> routerFunction(
             RegisterUserHandlerV1 registerUserHandlerV1,
             GetUserByIdHandlerV1 getUserByIdHandlerV1,
-            GetUserByIdentityDocumentHandlerV1 getUserByIdentityDocumentHandlerV1
+            GetUserByIdentityDocumentHandlerV1 getUserByIdentityDocumentHandlerV1,
+            ListUsersByIdentityDocumentsHandlerV1 listUsersByIdentityDocumentsHandlerV1
     ) {
         return RouterFunctions
                 .route()
                 .path("/api/v1/usuarios", builder ->
                         builder
                                 .POST("", registerUserHandlerV1::handle)
+                                .POST("/identity-documents", listUsersByIdentityDocumentsHandlerV1::handle)
                                 .GET("identity-document/{identityDocument}", getUserByIdentityDocumentHandlerV1::handle)
                                 .GET("/{id}", getUserByIdHandlerV1::handle)
                 ).build();
